6687e7e9f2b661058f7a7ea97e37ec2e7852cd21
unknown <agarwal.utkarsh2010@gmail.com>
2020-05-29 14:54:08 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ const messageTwo = document.querySelector('#messageTwo')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = searchAddress.value
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                messgaeOne.textContent=data.error
